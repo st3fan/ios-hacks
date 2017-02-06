8fa342e716b8f3507b25d76ef02747e24011a72d
Stefan Arentz <stefan@arentz.ca>
2017-02-06 12:24:30 -0500

    Updates to ios-locales.py

--- ios-locales.py
+++ ios-locales.py
@@ -66,35 +66,39 @@ if __name__ == "__main__":
     #    print("    " + runtime_root(rt[1]))
     #sys.exit(0)
 
-    locales_per_runtime = dict()
-    all_locales = set()
+    # Tier 1
+
+    locales1_per_runtime = dict()
+    all_locales1 = set()
     for rt in ios_runtimes():
         rt_version, rt_path = rt
-        locales_per_runtime[rt_version] = locales1_for_runtime(rt_path)
-        for locale in locales_per_runtime[rt_version]:
-            all_locales.add(locale)
+        locales1_per_runtime[rt_version] = locales1_for_runtime(rt_path)
+        for locale in locales1_per_runtime[rt_version]:
+            all_locales1.add(locale)
 
     print("<h1>iOS Locales</h1>")
     print("<p><i>Stefan Arentz, February 2017</i></p>")
     print('<p>Generated with <a href="https://github.com/st3fan/ios-hacks/blob/master/ios-locales.py">github.com/st3fan/ios-hacks/ios-locales.py</a></p>')
 
-    print("<h2>iOS Tier-1 Locales</h2>")
+    print("<h2>iOS Tier-1 Locales (%d)</h2>" % len(all_locales1))
     print("<table width='75%'>")
     print("<tr>")
     print("<td></td>")
-    for runtime_version in natural_sort(locales_per_runtime.keys()):
+    for runtime_version in natural_sort(locales1_per_runtime.keys()):
         print("<td>%s</td>" % runtime_version)
     print("</tr>")
-    for locale in sorted(all_locales):
+    for locale in sorted(all_locales1):
         print("<tr>")
         print("<td>%s</td>" % locale)
-        for runtime_version in locales_per_runtime.keys():
-            if locale in natural_sort(locales_per_runtime[runtime_version]):
+        for runtime_version in locales1_per_runtime.keys():
+            if locale in natural_sort(locales1_per_runtime[runtime_version]):
                 print("<td>%s</td>" % locale)
             else:
                 print("<td>-</td>")
         print("</tr>")
     print("</table>")
+
+    # Tier 2
 
     locales_per_runtime = dict()
     all_locales = set()
@@ -104,7 +108,7 @@ if __name__ == "__main__":
         for locale in locales_per_runtime[rt_version]:
             all_locales.add(locale)
 
-    print("<h2>iOS Tier-2 Locales</h2>")
+    print("<h2>iOS Tier-2 Locales (%d)</h2>" % len(all_locales))
     print("<table width='75%'>")
     print("<tr>")
     print("<td></td>")
